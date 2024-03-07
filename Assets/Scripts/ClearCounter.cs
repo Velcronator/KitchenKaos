216a3808e676012dfa1e4d92e3eb7d68c89b3541
velcronator <valacos7@gmail.com>
2024-03-07 12:10:20 +1100

    Player Pick up, Drop Objects: Many Prefabs for kitchen Items and counters

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -8,6 +8,27 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player player)
     {
-        
+        if (!HasKitchenObject())
+        {// Counter Empty
+            if (player.HasKitchenObject())
+            {// Player has stuff hand it over
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {// Player and counter doesn't have stuff do nothing
+
+            }
+        }
+        else
+        {// Stuff already there
+            if(player.HasKitchenObject())
+            {// player has stuff then do nothing
+
+            }
+            else
+            {// player doesn't have stuff.
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
